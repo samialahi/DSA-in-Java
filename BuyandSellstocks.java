092d53ae76084419ea70ad0cd9a664bdc751e029
Sami Alahi <samialahi1212@gmail.com>
2024-05-11 19:52:20 +0600

    BuyandSellstocks.java

---
+++ BuyandSellstocks.java
@@ -0,0 +1,22 @@
+public class BuyAndSellStock {
+    public static int maxProfit(int[] prices) {
+        int buyPrice = prices[0];
+        int profit = 0;
+
+        for (int i = 1; i < prices.length; i++) {
+            if (buyPrice < prices[i]) {
+                int profitToday = prices[i] - buyPrice;
+                profit = Math.max(profitToday, profit);
+            }
+            else {
+                buyPrice = prices[i];
+            }
+        }
+
+        return profit;
+    }
+    public static void main(String args[]) {
+        int prices[] = {7,1,5,3,6,4};
+        System.out.println(maxProfit(prices));
+    }
+}
